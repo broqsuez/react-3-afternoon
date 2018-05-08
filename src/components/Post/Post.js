050d3f56703f9df4f3779f64e906c30814db9fd8
Brock Sues <broqsuez@gmail.com>
2018-05-08 15:27:16 -0500

    completed exercise

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -54,7 +54,7 @@ export default class Post extends Component {
       // const editing = this.state.editing
       // const showMasterMenu = this.state.showMasterMenu
     const { editing, showMasterMenu } = this.state;
-
+    const { text, date, id } = this.props;
     return (
       // Main body of post
       <section className="Post__parent" onClick={ this.hideMasterMenu }>
@@ -66,7 +66,7 @@ export default class Post extends Component {
           {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
           <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
             <span onClick={ this.showEdit }>Edit</span>
-            <span>Delete</span>
+            <span onClick={ () => this.props.deletePostFn( id ) }>Delete</span>
           </div>
         </div>
 
@@ -79,7 +79,7 @@ export default class Post extends Component {
           <span className="Post__name">DevMountain</span>
           <span className="Post__handle">@DevMountain</span>
 
-          <span className="Post__date">- POST DATE GOES HERE</span>
+          <span className="Post__date"> { date } </span>
         </div>
 
         {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
@@ -91,16 +91,19 @@ export default class Post extends Component {
               }
         */}
         <div className="Post__content">
-          {
-            // This has been pulled off of this.state via destructuring
-            editing
-            ?
-              <Edit text=""
-                    hideEdit={ this.hideEdit } />
-            :
-              <span className="Post__text">POST TEXT GOES HERE</span>
-          }
-        </div>
+  {
+    editing
+    ?
+      <Edit text={ text }
+            id={ this.props.id } // Remember to destructure id off of props or use this.props.id
+            hideEdit={ this.hideEdit }
+            updatePostFn={ this.props.updatePostFn }
+            deletePostFn={ this.props.deletePostFn }
+            createPost= {this.props.createPost} />
+    :
+      <span className="Post__text">{ text }</span>
+  }
+</div>
 
         {/* These are all of the cute little icons in the bottom left corner */}
         <div className="Post__user-controls">
